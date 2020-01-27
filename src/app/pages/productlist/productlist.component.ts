dc26d7b83123ba1a77ce8996fe62bae27955399b
gokayay <gokayay98@gmail.com>
2020-01-27 17:17:10 +0300

    filtering-on-products

--- src/app/pages/productlist/productlist.component.ts
+++ src/app/pages/productlist/productlist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { DataService } from 'app/data.service';
 import { Product } from 'app/models/product.model';
+import { HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -13,16 +14,26 @@ export class ProductListComponent implements OnInit{
    
     products: Product[];
     @Input() page: string;
+    @Input() size: string;
+    @Input() pageSize: string;
+
+
+    httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json', 'size' : this.size , 'pageSize' : this.pageSize })};
+    //const httpHeaders =new HttpHeaders({ 'Content-Type': 'application/json', 'size' : this.size , 'pageSize' : this.pageSize });
 
     passPage(paged){
       console.log(paged);
       this.dataService.getProducts(paged)
-      .subscribe(data =>this.products = data);
+      .subscribe(data =>{this.products = data;
+      });
     }
  
     constructor(private dataService: DataService){}
   
     ngOnInit(){
+      this.size="17";
+      this.pageSize="3";
+
       this.dataService.getProducts(this.page)
       .subscribe(data =>this.products = data);
     }
